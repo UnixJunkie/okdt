bfc2549b280aca08ffc6a6aee6129522c3375240
Francois BERENGER <ligand@free.fr>
2018-02-02 18:01:05 +0900

    WIP

--- kd_tree.ml
+++ kd_tree.ml
@@ -1,4 +1,17 @@
-(* kd-tree for d-dimensional points *)
+(* kd-tree: a tree to store k-dimensional points
+
+   Cf. chapter 5.2 in:
+
+   @book{CompGeomThirdEdSpringer,
+   title     = "Computational Geometry: Algorithms and Applications",
+   author    = "M. {de Berg} and O. Cheong and M. {van Kreveld} and
+   M. Overmars",
+   edition   = "Third Edition",
+   pages     = {223--224},
+   doi       = "10.1007/978-3-540-77974-2",
+   year      = "2008",
+   publisher = "Springer"
+   } *)
 
 module A = Array
 module L = List
@@ -11,9 +24,57 @@ module type Point = sig
   val ndims: int
   (* get coord in given dimension *)
   val get_coord: int -> t -> float
+  (* point of coords *)
+  val create: float array -> t
 end
 
 module Make = functor (P: Point) -> struct
+
+  (* a k-dimensional box *)
+  module Range = struct
+    type t = { lb: P.t ; (* left bound *)
+               rb: P.t } (* right bound *)
+    let create lb rb =
+      { lb; rb }
+    (* get bounds in given dimension *)
+    let get_bounds ndim r =
+      (P.get_coord ndim r.lb, P.get_coord ndim r.rb)
+    (* tighten lbound in given dimension *)
+    let tighten_lbound ndim new_bound box =
+      let new_coords = A.init P.ndims (fun i -> P.get_coord i box.lb) in
+      new_coords.(ndim) <- new_bound;
+      { box with lb = P.create new_coords }
+    (* tighten rbound in given dimension *)
+    let tighten_rbound ndim new_bound box =
+      let new_coords = A.init P.ndims (fun i -> P.get_coord i box.rb) in
+      new_coords.(ndim) <- new_bound;
+      { box with rb = P.create new_coords }
+    let min_corner () =
+      P.create (A.init P.ndims (fun _ -> -.max_float))
+    let max_corner () =
+      P.create (A.init P.ndims (fun _ -> max_float))
+    let max_box () =
+      { lb = min_corner () ;
+        rb = max_corner () }
+    exception Outside_bounds
+    let point_is_inside p box =
+      try
+        for i = 0 to P.ndims - 1 do
+          let y = P.get_coord i p in
+          let x, z = get_bounds i box in
+          if y < x || y > z then raise Outside_bounds
+        done;
+        true
+      with Outside_bounds -> false
+    (* a small box is completely inside a big one if all its corners are inside *)
+    let is_inside small big =
+      (* FBR: I need a function to list all the corners of a box *)
+      failwith "not implemented yet"
+    exception Intersect
+    (* two cubes intersect if any corner of any cube is inside the other cube *)
+    let intersect small big =
+      failwith "not implemented yet"
+  end
 
   type t = Empty
          | Leaf of P.t
@@ -95,8 +156,28 @@ module Make = functor (P: Point) -> struct
     loop 0 t
 
   (* "orthogonal range query": return all points whose coordinates
-     are inside [range] *)
-  let search range tree =
-    failwith "not implemented yet"
+     are inside [range] (a k-dimensional box) *)
+  let search range t =
+    let rec loop depth acc curr_box = function
+      | Empty -> acc
+      | Leaf p -> if Range.point_is_inside p range then p :: acc else acc
+      | Node (left, med, right) ->
+        let dim = depth mod P.ndims in
+        let depth' = depth + 1 in
+        let lbox = Range.tighten_rbound dim med curr_box in
+        let acc' =
+          if Range.is_inside lbox range then
+            to_list_loop acc left
+          else if Range.intersect lbox range then
+            loop depth' acc lbox left
+          else acc in
+        let rbox = Range.tighten_lbound dim med curr_box in
+        if Range.is_inside rbox range then
+          to_list_loop acc' right
+        else if Range.intersect rbox range then
+          loop depth' acc' rbox right
+        else acc' in
+    let max_box = Range.max_box () in
+    loop 0 [] max_box t
 
 end
